979cc4373750315e72608a7550b610e5c392e0f8
Aaron-Uriel <therialaaronuriel@gmail.com>
2021-05-27 20:03:24 -0500

    Cambiar tipos a unos mas realistas También agregué una coma que se me había olvidados xd

--- migrations/2021-02-12-185352_create_orders/up.sql
+++ migrations/2021-02-12-185352_create_orders/up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS orders (
     order_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     client_id INTEGER UNSIGNED NOT NULL,
-    food_id INTEGER UNSIGNED NOT NULL,
-    food_amount INTEGER UNSIGNED NOT NULL,
-    variant_id VARCHAR(2) NOT NULL,
+    food_id SMALLINT UNSIGNED NOT NULL,
+    food_amount TINYINT UNSIGNED NOT NULL,
+    variant_id VARCHAR(3) NOT NULL,
     is_finished BOOLEAN DEFAULT FALSE NOT NULL,
     CONSTRAINT `client_name_fk`
         FOREIGN KEY (client_id) REFERENCES clients (client_id)
--- src/schema.rs
+++ src/schema.rs
@@ -1,18 +1,18 @@
 table! {
     clients (client_id) {
         client_id -> Unsigned<Integer>,
-        client_name -> Varchar,
+        table_id -> Unsigned<Tinyint>,
         arrival -> Timestamp,
     }
 }
 
 table! {
     foods (food_id) {
-        food_id -> Unsigned<Integer>,
+        food_id -> Unsigned<Smallint>,
         f_name -> Varchar,
         price -> Decimal,
         estimated_time_minutes -> Nullable<Unsigned<Tinyint>>,
-        variant_group -> Unsigned<Integer>,
+        variant_group -> Unsigned<Tinyint>,
     }
 }
 
@@ -20,10 +20,17 @@ table! {
     orders (order_id) {
         order_id -> Unsigned<Integer>,
         client_id -> Unsigned<Integer>,
-        food_id -> Unsigned<Integer>,
-        food_amount -> Unsigned<Integer>,
+        food_id -> Unsigned<Smallint>,
+        food_amount -> Unsigned<Tinyint>,
         variant_id -> Varchar,
         is_finished -> Bool,
+    }
+}
+
+table! {
+    tables (table_id) {
+        table_id -> Unsigned<Tinyint>,
+        is_occupied -> Bool,
     }
 }
 
@@ -31,7 +38,7 @@ table! {
     variants (v_id) {
         v_id -> Varchar,
         v_name -> Varchar,
-        v_group -> Unsigned<Integer>,
+        v_group -> Unsigned<Tinyint>,
     }
 }
 
@@ -43,6 +50,7 @@ table! {
     }
 }
 
+joinable!(clients -> tables (table_id));
 joinable!(orders -> clients (client_id));
 joinable!(orders -> foods (food_id));
 joinable!(orders -> variants (variant_id));
@@ -51,6 +59,7 @@ allow_tables_to_appear_in_same_query!(
     clients,
     foods,
     orders,
+    tables,
     variants,
     workers,
 );
